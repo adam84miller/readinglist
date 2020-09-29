4c2458c2a7be1586fc38b28e29fc60fecc02765b
adam84miller <adam.84.miller@gmail.com>
2020-09-29 10:56:41 -0600

    Read function added and routed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
+const bookRouter = require('./routes/api/books');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/v1/books', bookRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
