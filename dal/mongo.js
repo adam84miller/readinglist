285548c8408465ada79f30b9c7db498ac5637c09
adam84miller <adam.84.miller@gmail.com>
2020-09-29 10:28:07 -0600

    Added data access layer

---
+++ dal/mongo.js
@@ -0,0 +1,64 @@
+const { MongoClient } = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
+
+let client;
+
+function connect(locals) {
+
+    const uri = process.env.DB_URL;
+
+    client = new MongoClient(uri, { useUnifiedTopology: true })
+
+    return client.connect()
+    .then((connection) => {
+        locals.collectionreadinglist = connection.db('readinglist').collection('readinglist')
+    })
+    .catch(err => {
+        console.log(err)
+        process.exit()
+    })
+
+}
+
+function close() {
+    client.close();
+}
+
+function readAll(info) {
+    return info.collection.find(info.query).toArray();
+}
+
+function readOne(info) {
+    return info.collection.findOne(info.query);
+}
+
+function readOneById(info) {
+    return info.collection.findOne({_id: ObjectID(info.id)});
+}
+
+/*function createOne(info) {
+    return info.collection.insertOne(info.doc);
+}
+
+function replaceOne(info) {
+    return info.collection.findOneAndReplace({ _id: ObjectID(info.id) }, info.doc);
+}
+
+function changeOne(info) {
+    return info.collection.findOneAndUpdate(info.query,{$set: info.doc});
+}
+
+function deleteOne(info) {
+    return info.collection.deleteOne({ _id: ObjectID(info.id) }); 
+}
+*/
+module.exports.connect = connect;
+module.exports.close = close;
+module.exports.readAll = readAll;
+module.exports.readOne = readOne;
+module.exports.readOneById = readOneById;
+/*module.exports.createOne = createOne;
+module.exports.replaceOne = replaceOne;
+module.exports.changeOne = changeOne;
+module.exports.deleteOne = deleteOne;
+*/
